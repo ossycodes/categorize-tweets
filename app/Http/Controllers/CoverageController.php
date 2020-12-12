da11ada7c77a43edacb8fe9ba3e8b6b9336afb34
ossycodes <ossyfizy009@gmail.com>
2020-12-12 15:40:47 +0100

    return bad response from CoverageController to ensure our tests workflows is working fine

--- app/Http/Controllers/CoverageController.php
+++ app/Http/Controllers/CoverageController.php
@@ -5,13 +5,9 @@ namespace App\Http\Controllers;
 class CoverageController extends Controller
 {
     public function index()
-    {
-        $array = [
-            "name" => "key",
-        ];
-        
+    {   
         return response()->json([
-            'status' => 'ok',
+            'status' => 'fails test',
         ]);
     }
 }
