7b0b9c01fbe3e7af6665b422191e1d1f5140d834
ossycodes <ossyfizy009@gmail.com>
2020-12-12 15:04:31 +0100

    set up basic api route, controller and tests

---
+++ app/Http/Controllers/CoverageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CoverageController extends Controller
+{
+    public function index() {
+        return response()->json([
+            'status' => 'ok'
+        ]);
+    }
+}
---
+++ tests/Feature/CoverageTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CoverageTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_access_coverage_index_method()
+    {
+        $response = $this->get('api/coverage');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'status' => 'ok'
+        ]);
+    }
+}
